0a2a8147b93751ecc85eced3dc43f324cfc09add
Abrar <hossainabrar@aol.com>
2024-08-01 22:44:50 -0400

    added project initial arguments and property method for client

--- lib/models/expense.py
+++ lib/models/expense.py
@@ -2,8 +2,5 @@ from models.__init__ import CONN, CURSOR
 
 class Expense:
 
-    # save all expenses
-    all_expenses = {}
-
     def __init__(self):
         pass
--- lib/models/project.py
+++ lib/models/project.py
@@ -5,6 +5,20 @@ class Project:
     # save all projects
     all_project = {}
 
-    def __init__(self):
-        pass
+    def __init__(self, name, client, location, quote, start_date = None, end_date = None):
+        self.name = name
+        self.client = client
+        self.location = location
+        self.quote = quote
+        self.start_date = start_date
+        self.end_date = end_date
 
+    @property
+    def client(self):
+        return self._client
+    @client.setter
+    def client(self, client):
+        if isinstance(client, str) and len(client):
+            self._client = client
+        else:
+            raise ValueError('Must enter clients name')
