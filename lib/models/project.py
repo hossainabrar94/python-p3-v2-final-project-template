3548f69b36091dbb0d2b69129e5fafe28ec0a77e
Abrar <hossainabrar@aol.com>
2024-08-02 00:32:29 -0400

    added all ORM methods to the project model

--- lib/models/project.py
+++ lib/models/project.py
@@ -64,3 +64,32 @@ class Project:
             raise ValueError('Please enter a valid amount')
         
     
+    def create_table(self):
+        pass
+
+    def drop_table(self):
+        pass
+
+    def save(self):
+        pass
+
+    def create(self):
+        pass
+
+    def update(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def get_all(self):
+        pass
+
+    def find_by_name(self):
+        pass
+
+    def expenses(self):
+        pass
+
+    def project_by_year(self):
+        pass
