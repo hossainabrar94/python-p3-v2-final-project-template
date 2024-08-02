d8dd3b9dcf930fa01b230497c73d66e17fb6939d
Abrar <hossainabrar@aol.com>
2024-08-01 22:32:00 -0400

    added models

---
+++ lib/models/expense.py
@@ -0,0 +1,9 @@
+from models.__init__ import CONN, CURSOR
+
+class Expense:
+
+    # save all expenses
+    all_expenses = {}
+
+    def __init__(self):
+        pass
---
+++ lib/models/project.py
@@ -0,0 +1,10 @@
+from models.__init__ import CURSOR, CONN
+
+class Project:
+
+    # save all projects
+    all_project = {}
+
+    def __init__(self):
+        pass
+
